05b7b94c83b0fb70673675efb1faeb62a8f3649d
Emily Boudreaux <thomas@boudreauxmail.com>
2025-12-05 14:26:22 -0500

    feat(TOML & Glaze): YAML -> TOML
    YAML is a horrid format with far too many edge cases. We have switched
    to TOML. Further, we have completly reworked the framework so that
    
    1. There is no longer any global config state. Config objects now must
    be passed between scopes by the caller. This will introduce some more
    friction but whill also make order of initialization clear
    2. Config objects are now strongly typed and there is a single sourth of
    truth for any given config object baked in using the some struct.

---
+++ src/config/include/fourdst/config/exceptions/exceptions.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+namespace fourdst::config::exceptions {
+    class ConfigError : public std::exception {
+    public:
+        ConfigError(const std::string & what): m_msg(what) {}
+
+        const char* what() const noexcept override {
+            return m_msg.c_str();
+        }
+    private:
+        std::string m_msg;
+    };
+
+    class ConfigSaveError final : public ConfigError {
+        using ConfigError::ConfigError;
+    };
+
+    class ConfigLoadError final : public ConfigError {
+        using ConfigError::ConfigError;
+    };
+
+    class ConfigParseError final : public ConfigError {
+        using ConfigError::ConfigError;
+    };
+
+    class SchemaGenerationError final : public ConfigError {
+        using ConfigError::ConfigError;
+    };
+
+    class SchemaNameError final : public ConfigError {
+        using ConfigError::ConfigError;
+    };
+
+}
---
+++ tests/config/test_schema.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+
+struct PhysicsConfigOptions {
+    bool diffusion;
+    bool convection;
+    bool radiation;
+    std::array<int, 3> flags;
+};
+
+struct SimulationConfigOptions {
+    double time_step = 1;
+    double total_time = 10;
+    int output_frequency = 1;
+};
+
+struct OutputConfigOptions {
+    std::string directory = "./output";
+    std::string format = "hdf5";
+    bool save_plots = false;
+};
+
+struct TestConfigSchema {
+    std::string description;
+    std::string author;
+
+    PhysicsConfigOptions physics;
+    SimulationConfigOptions simulation;
+    OutputConfigOptions output;
+};
